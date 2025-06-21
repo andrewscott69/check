34a3823a40e8f4da8b5f12daa27f70e80043ad79
andrewscott69 <contactandrewscott69@gmail.com>
2025-06-21 11:28:04 +0100

    Update page.tsx

--- app/loans/page.tsx
+++ app/loans/page.tsx
@@ -11,7 +11,7 @@ export default function LoansPage() {
       <section className="relative w-full h-[400px] bg-gradient-to-r from-emerald-900 to-green-900">
         <div className="absolute inset-0 z-0">
           <Image
-            src="/personalbanking.png"
+            src="/happycouple2.jpg"
             alt="Personal Loans"
             fill
             className="object-cover opacity-40 mix-blend-overlay"

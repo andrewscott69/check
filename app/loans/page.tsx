28c383eeddf0013302179b2dbfebeb934e85cce0
andrewscott69 <contactandrewscott69@gmail.com>
2025-06-25 22:04:22 +0100

    Update page.tsx

--- app/loans/page.tsx
+++ app/loans/page.tsx
@@ -26,7 +26,10 @@ export default function LoansPage() {
               loan solution for you.
             </p>
             <Button size="lg" className="bg-yellow-500 hover:bg-yellow-600 text-white font-medium">
-              Apply for a Loan <ChevronRight className="ml-1 h-4 w-4" />
+              <Link href="/u/login">
+                  Apply for a Loan <ChevronRight className="ml-1 h-4 w-4" />
+                </Link>
+              
             </Button>
           </div>
         </div>

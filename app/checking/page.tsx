f87bb92dabb4d4c0952dcf1f4132e7595d39eb22
andrewscott69 <contactandrewscott69@gmail.com>
2025-06-25 18:55:22 +0100

    Update page.tsx

--- app/checking/page.tsx
+++ app/checking/page.tsx
@@ -25,7 +25,10 @@ export default function CheckingPage() {
               fit your needs.
             </p>
             <Button size="lg" className="bg-yellow-500 hover:bg-yellow-600 text-white font-medium">
-              Open Account <ChevronRight className="ml-1 h-4 w-4" />
+              <Link href="/u/login" className="text-gray-300 hover:text-white">
+                  Checking Accounts
+                </Link>
+            
             </Button>
           </div>
         </div>

5c76499aaa81dede4f5c8be0d8cc64761a953c6e
andrewscott69 <contactandrewscott69@gmail.com>
2025-07-07 10:18:46 -0700

    Update route.ts

--- app/api/contact/route.ts
+++ app/api/contact/route.ts
@@ -10,8 +10,8 @@ export async function POST(req: Request) {
 
   const transporter = nodemailer.createTransport({
     host: process.env.EMAIL_SERVER_HOST,
-    port: parseInt(process.env.EMAIL_SERVER_PORT || "587"),
-    secure: false,
+    port: parseInt(process.env.EMAIL_SERVER_PORT || "465"),
+    secure: true,
     auth: {
       user: process.env.EMAIL_SERVER_USER,
       pass: process.env.EMAIL_SERVER_PASSWORD,
